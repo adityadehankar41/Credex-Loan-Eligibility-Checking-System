72a70758bfb45489a2331bd31211b4ef10379246
Aditya Dehankar <adityadehankar41@gmail.com>
2026-06-17 23:54:21 +0530

    update

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ function showLoanResult(prediction, phone) {
   document.getElementById('loan-result-modal').classList.add('open');
 
   // ✅ WHATSAPP OPEN
-  fetch('http://localhost:8000/send-whatsapp', {
+  fetch('https://credex-loan-eligibility-checking-system.onrender.com/send-whatsapp', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
